02d69c4b0751da18c35e22a09038356fde480181
PeppaLokie <christianjuan17@gmail.com>
2025-08-02 17:12:47 +0800

    Refactor code structure for improved readability and maintainability

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 let lastScrollY = window.scrollY;
-const logo = document.getElementById('navbarLogo');
-const buttons = document.getElementById('navbarButtons');
+const logo = document.getElementById("navbarLogo");
+const buttons = document.getElementById("navbarButtons");
+const hamburger = document.getElementById("hamburgerMenu");
+const navLinks = document.querySelector(".navbar-links");
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   const currentScrollY = window.scrollY;
 
   if (currentScrollY > 150 && currentScrollY > lastScrollY) {
-    logo.classList.add('hide-up');
-    buttons.classList.add('hide-up');
+    logo.classList.add("hide-up");
+    buttons.classList.add("hide-up");
   } else if (currentScrollY < lastScrollY) {
-    logo.classList.remove('hide-up');
-    buttons.classList.remove('hide-up');
+    logo.classList.remove("hide-up");
+    buttons.classList.remove("hide-up");
   }
 
   lastScrollY = currentScrollY;
+});
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
 });
